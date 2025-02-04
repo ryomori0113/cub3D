131ee1bbf60de4120bfb870d2e20eedd29557537
Mori0113 <ruhakaforest.kaki.88@gmail.com>
2025-02-04 20:27:17 +0900

    test_map追加

--- cub3D_dir/tester/test.sh
+++ cub3D_dir/tester/test.sh
@@ -3,7 +3,7 @@ target_dir=".."
 
 cd $target_dir && make re
 
-directory="./maps/ng"
+directory="./maps/NG_MAP"
 
 find "$directory" -type f | sort | while read file; do
     # 各ファイルに対して ./cub3D コマンドを実行
