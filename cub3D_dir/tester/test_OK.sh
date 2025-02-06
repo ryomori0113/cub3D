8918e7fb51251ff9bf486a8cc46f4f1324b11c85
Mori0113 <ruhakaforest.kaki.88@gmail.com>
2025-02-06 17:08:07 +0900

    テストケース追加

---
+++ cub3D_dir/tester/test_OK.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+target_dir=".."
+
+cd $target_dir && make re
+
+directory="./maps/OK_MAP"
+
+find "$directory" -type f | sort | while read file; do
+    # 各ファイルに対して ./cub3D コマンドを実行
+    echo "-----------------------"
+	echo "pattern: $file"
+    valgrind -q ./Cub3D "$file"
+    # ./Cub3D "$file"
+	echo "-----------------------"
+done
